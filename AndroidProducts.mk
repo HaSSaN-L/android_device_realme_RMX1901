8e26089596fa99147e199d1b86ccc7cbea42ff4d
HaSSaN-L <64253455+HaSSaN-L@users.noreply.github.com>
2020-07-14 00:12:24 -0700

    RMX1901: Bringup ArrowOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1901.mk
+    $(LOCAL_DIR)/arrow_RMX1901.mk
+COMMON_LUNCH_CHOICES := \
+    arrow_RMX1901-user \
+    arrow_RMX1901-userdebug \
+    arrow_RMX1901-eng
